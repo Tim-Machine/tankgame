6c4e9036764a35cbfa28e1326511359009714a86
Tim Smith <tsmith2686@gmail.com>
2017-02-25 12:05:54 -0500

    added game

---
+++ tankGame/Source/tankGame/tankGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "tankGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAME_API AtankGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
