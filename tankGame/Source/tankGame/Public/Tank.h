59255771f9cad032d94893a673a5e25ce5e9088d
Tim Smith <tsmith2686@gmail.com>
2017-02-28 19:06:43 -0500

    getting  location of barrel for targeting

--- tankGame/Source/tankGame/Public/Tank.h
+++ tankGame/Source/tankGame/Public/Tank.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -12,10 +13,14 @@ class TANKGAME_API ATank : public APawn
 
 public:
 	void AimAt(FVector Hitlocation);
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 
 private:
 	// Sets default values for this pawn's properties
@@ -26,6 +31,4 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
 };
